8916e2664e57b573fdd236f91cea7f5523bbc918
unknown <areyad203011@bscse.uiu.ac.bd>
2024-04-29 20:53:44 +0600

    delete function done

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/crafts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await artCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     app.put('/crafts/:id', async (req, res) => {
       const id = req.params.id;
